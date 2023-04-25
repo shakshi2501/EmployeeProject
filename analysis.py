464d87c06faf9e21c85b6ff0f87797825f9402a3
shakshi2501 <131715431+shakshi2501@users.noreply.github.com>
2023-04-25 19:24:45 +0530

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,14 +1,43 @@
 import mysql.connector as mq
 import matplotlib.pyplot as plt
-import seaborn as sb
+import tkinter as tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+
 import pandas as pd
-class Analyse:
-    con = mq.connect(host='localhost', database='emp_db', user='root', password='sunrise.123')
-    def __init__(self):
-        q = 'Select * from emp_table'
-        df = pd.read_sql(q,self.con)
 
+con = mq.connect(host='localhost', database='emp_db', user='root', password='sunrise.123')
 
-        plt.pie(df.groupby(['PerformanceRating']).size(), labels=df.groupby(['PerformanceRating']).size())
-        plt.show()
-       # print(df.groupby(['EmpJobRole']).sum().plot(kind='pie', y='PerformanceRating'))
+def analyse():
+    q = 'Select * from emp_table'
+    df = pd.read_sql(q, con)
+    return df
+
+def show_chart(df, option):
+    window = tk.Tk()
+    window.title("Data Analysis")
+    window.geometry('805x450+388+154')
+    if option == 'Employee Performance Analysis on the basis of Department':
+        figure = plt.Figure(figsize=(10, 10), dpi=100)
+        ax = figure.add_subplot(221)
+        chart_type = FigureCanvasTkAgg(figure, window)
+        chart_type.get_tk_widget().pack()
+        gdf = df[['EmpDepartment', 'PerformanceRating']].groupby('EmpDepartment').sum()
+        gdf.plot(kind='bar', legend=True, ax=ax)
+
+    elif option == 'Job Involvement Analysis on the basis of Department':
+        figure = plt.Figure(figsize=(30, 20), dpi=80)
+        ax = figure.add_subplot(221)
+        chart_type = FigureCanvasTkAgg(figure, window)
+        chart_type.get_tk_widget().pack()
+        gdf = df[['EmpDepartment', 'EmpJobInvolvement']].groupby('EmpDepartment').sum()
+        gdf.plot(kind='bar', legend=True, ax=ax)
+
+    elif option == 'Job Satisfaction Analysis on the basis of Department':
+        figure = plt.Figure(figsize=(30, 20), dpi=80)
+        ax = figure.add_subplot(221)
+        chart_type = FigureCanvasTkAgg(figure, window)
+        chart_type.get_tk_widget().pack()
+        gdf = df[['EmpDepartment', 'EmpJobSatisfaction']].groupby('EmpDepartment').sum()
+        gdf.plot(kind='bar', legend=True, ax=ax)
+
